3223fceb9e34cfec185826e9dc8674684b62ccfd
Nahush Farkande <john@fortella.ai>
2022-03-19 01:00:09 -0400

    projects error fix

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -4,35 +4,9 @@ import { AnimatedBlob } from '../../components/Blob';
 import { ProjectList } from '../../components/ProjectList';
 import { BackButton, Navigation } from '../../components/reusableComponents';
 import { maxDevice } from '../../constants/styles';
+import { PROJECTS } from '../../constants/projects';
 import { useIsDevice } from '../../useWindowSize';
-
-const projects: IProject[] = [
-    {
-        id: 2,
-        title: 'Farm2U',
-        description: 'Connecting people to their local produce in an efficient manner',
-        gradient: ['#58B96E', '#7DBD78'],
-        image: 'Farm2U-splash.png',
-        route: '/projects/farm2u'
-    },
-    {
-        id: 3,
-        title: 'Trupaws',
-        description: 'Matching adopters with shelter animals',
-        gradient: ['#F7516F', '#FED089'],
-        image: 'Trupaws-splash.png',
-        route: '/projects/trupaws'
-    },
-    {
-        id: 1,
-        title: 'Courtside',
-        description: 'Enhancing the basketball experience for the visually impaired',
-        gradient: ['#F4A58A', '#ED6B4E'],
-        image: 'Courtside-splash.png',
-        route: '/projects/courtside'
-    }
-];
-
+const projects = PROJECTS;
 const Projects = () => {
     const [selectedProject, setSelectedProject] = useState(projects[2]);
     const openId = selectedProject?.id || -1;
@@ -45,16 +19,16 @@ const Projects = () => {
                     openId={openId}
                     onMobileProjectChange={(project) => isMobile && setSelectedProject(project)}
                     onProjectMouseOver={(project) => {
-                    if (!isMobile) {
-                        if (openId !== project.id) {
-                            setSelectedProject(project);
-                    }
-                    }
+                        if (!isMobile) {
+                            if (openId !== project.id) {
+                                setSelectedProject(project);
+                            }
+                        }
                     }}
                     onProjectMouseLeave={() => !isMobile && setSelectedProject(null)}
                 />
-                    <h1>Projects</h1>
-                    <BackButton href="/" />
+                <h1>Projects</h1>
+                <BackButton href="/" />
             </div>
             <div className="project-presentation">
                 {selectedProject && <img src={`/${selectedProject?.image}`} />}
