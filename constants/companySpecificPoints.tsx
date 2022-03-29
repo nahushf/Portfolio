a9134f3d26bef34fefc2a3809539e2a840d7efa3
Nahush Farkande <john@fortella.ai>
2022-03-29 03:52:49 -0400

    add square trade

--- constants/companySpecificPoints.tsx
+++ constants/companySpecificPoints.tsx
@@ -1,6 +1,7 @@
 import roLogo from '../public/ro-logo.png';
 import goDaddyLogo from '../public/GoDaddy-Logo.png';
 import abelsonTaylorLogo from '../public/abelson-taylor-logo.png';
+import squareTradeLogo from '../public/square-trade-logo.png';
 
 interface ICompanyPoint {
     title: string;
@@ -96,7 +97,7 @@ export const COMPANIES = [
     new Company('Abelson Taylor', 'abelson-taylor', abelsonTaylorLogo, 160)
         .setCompanyPoints((companyName) => [
             {
-                title: 'I see the value in the and healthcare domain',
+                title: 'I see the value in the healthcare domain',
                 description: `
                 Healthcare was always a rich field for me (in terms of the challenges and the room for design
                 innovation). But Covid has made it clear that telehealth is the need of the hour and could use some
@@ -111,6 +112,41 @@ export const COMPANIES = [
                 title: 'I want to understand the challenges in the healthcare domain',
                 description:
                     'This opportunity would expose me to the world of healthcare and allow me to learn and empathize with users in a domain that I am not too experienced in.'
+            }
+        ])
+        .setMePoints([
+            {
+                title: 'Philosophy: Be the dumbest person in the room',
+                description:
+                    'I am a strong believer in the "dumbest person in the room philosophy". If one wishes to grow he needs to work with people who are more skilled.'
+            },
+            {
+                title: 'Quick Learner',
+                description:
+                    'I have a passion for learning and often enjoy being thrown into environments I possess little to no knowledge of'
+            },
+            {
+                title: 'Extensive closely related experience',
+                description:
+                    'Even thought I am just starting my professional design journey, I have worked and led multiple teams in the past 6 years when I worked as a front end developer and collaborated with multiple of UI/UX and Product Designers'
+            }
+        ]),
+    new Company('SquareTrade', 'square-trade', squareTradeLogo, 200)
+        .setCompanyPoints((companyName) => [
+            {
+                title: 'I see the value in the device-protection and warranty domain',
+                description: `
+                It is no surprise that the number of devices per person is growing at an exponential pace. But with the increase in the new devices, the accidents with those devices have grown at an even faster rate. It is evident how important the field of device protection is and this is the reason why I wish to design products in this domain.
+            `
+            },
+            {
+                title: 'I wish to learn about the new and upcoming innovations in this domain',
+                description: `I am curious to know what's next in this domain. As this market will continue to grow, it will keep bringing up new challenges and I wish to learn and contribute to this problems.`
+            },
+            {
+                title: 'I want to understand the challenges in the warranty domain',
+                description:
+                    'This opportunity would expose me to the world of device protection and warranty and allow me to learn and empathize with users in a domain that I am not too experienced in.'
             }
         ])
         .setMePoints([
