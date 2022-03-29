97c9dd8b0b586ef58ed1fe0dd106e8bacce052ad
Nahush Farkande <john@fortella.ai>
2022-03-29 03:38:52 -0400

    added Abelson Taylor

--- constants/companySpecificPoints.tsx
+++ constants/companySpecificPoints.tsx
@@ -1,5 +1,6 @@
 import roLogo from '../public/ro-logo.png';
 import goDaddyLogo from '../public/GoDaddy-Logo.png';
+import abelsonTaylorLogo from '../public/abelson-taylor-logo.png';
 
 interface ICompanyPoint {
     title: string;
@@ -73,6 +74,43 @@ export const COMPANIES = [
             {
                 title: `I wish to understand ${companyName}'s users and their challenges`,
                 description: `I am curious about what sort of challenges ${companyName}'s users face. I am aware of how and what the company works on and as someone who has not worked in the web-domain sale market, I wish to understand and empathize with the users of the service. `
+            }
+        ])
+        .setMePoints([
+            {
+                title: 'Philosophy: Be the dumbest person in the room',
+                description:
+                    'I am a strong believer in the "dumbest person in the room philosophy". If one wishes to grow he needs to work with people who are more skilled.'
+            },
+            {
+                title: 'Quick Learner',
+                description:
+                    'I have a passion for learning and often enjoy being thrown into environments I possess little to no knowledge of'
+            },
+            {
+                title: 'Extensive closely related experience',
+                description:
+                    'Even thought I am just starting my professional design journey, I have worked and led multiple teams in the past 6 years when I worked as a front end developer and collaborated with multiple of UI/UX and Product Designers'
+            }
+        ]),
+    new Company('Abelson Taylor', 'abelson-taylor', abelsonTaylorLogo, 160)
+        .setCompanyPoints((companyName) => [
+            {
+                title: 'I see the value in the and healthcare domain',
+                description: `
+                Healthcare was always a rich field for me (in terms of the challenges and the room for design
+                innovation). But Covid has made it clear that telehealth is the need of the hour and could use some
+                innovation as well.
+            `
+            },
+            {
+                title: 'I wish to work in a design mature company',
+                description: `${companyName}'s values, stature and growth attracts a lot of talent and would allow me to work with and learn from designers at the top of their game.`
+            },
+            {
+                title: 'I want to understand the challenges in the healthcare domain',
+                description:
+                    'This opportunity would expose me to the world of healthcare and allow me to learn and empathize with users in a domain that I am not too experienced in.'
             }
         ])
         .setMePoints([
