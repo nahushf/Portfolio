a1336aabe0414462c16558de8bf7b581e80b01c7
Nahush Farkande <john@fortella.ai>
2022-04-14 14:36:36 -0400

    added tilt effect

--- components/ProjectTile.tsx
+++ components/ProjectTile.tsx
@@ -3,6 +3,10 @@ import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import { CUSTOM_EASING } from '../constants/styles';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { fa500px } from '@fortawesome/free-brands-svg-icons';
+
+const Tilt = require('react-parallax-tilt').default;
 
 const variants = {
     rest: { transform: 'scale(1)', boxShadow: '0px 0px 0px transparent' },
@@ -35,12 +39,19 @@ const cardVariants = {
     active: { background: 'transparent' }
 };
 
+console.log(Tilt);
+
 export const ProjectTile = ({
     project: { route, gradient, image, title, description, tags, badges }
 }: {
     project: IProject;
 }) => {
     const router = useRouter();
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+    const ImageWrapper = isMounted ? Tilt : 'div';
     return (
         <Container
             initial="rest"
@@ -53,12 +64,20 @@ export const ProjectTile = ({
             className="project-tile"
         >
             <div className="project-information">
-                <motion.div
-                    className="image-container"
+                <ImageWrapper
+                    className="image-container parallax-effect-glare-scale"
                     style={{ background: `linear-gradient(137.49deg, ${gradient[0]} 0%, ${gradient[1]} 96.01%)` }}
                     variants={variants}
+                    glareEnable={true}
+                    glareMaxOpacity={0.6}
+                    gyroscope={true}
+                    scale={1.02}
+                    transitionSpeed={1500}
+                    tiltMaxAngleY={10}
+                    perspective={1000}
+                    tiltMaxAngleX={10}
                 >
-                    <img src={image} />
+                    <img src={image} className="inner-element" style={{ transform: 'translateZ(60px)' }} />
                     <div className="badges-container">
                         {badges?.length &&
                             badges.map(({ img, tagline }, index) => (
@@ -68,7 +87,7 @@ export const ProjectTile = ({
                                 </div>
                             ))}
                     </div>
-                </motion.div>
+                </ImageWrapper>
                 <div className="project-title">{title}</div>
                 <div className="project-description">{description}</div>
                 <div className="tags-container">
@@ -93,6 +112,7 @@ const Container = styled(motion.div)`
     cursor: pointer;
     border-radius: 12px;
     padding: 8px;
+    cursor: pointer;
     &:hover {
         background: #291d37;
     }
@@ -116,14 +136,20 @@ const Container = styled(motion.div)`
         display: flex;
         justify-content: center;
         align-items: start;
+        transform-style: preserve-3d;
+        .glare-wrapper {
+            border-radius: 16px !important;
+        }
         img {
-            width: 100%;
+            width: 90%;
+            transform: translateZ(60px);
         }
         .badges-container {
             position: absolute;
             bottom: 16px;
             left: 16px;
             display: flex;
+            transform: translateZ(60px);
             .badge-container {
                 display: flex;
                 position: relative;
